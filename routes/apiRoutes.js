51d83b8334503ba7832252d43cb81d7cbc3262be
colint771 <colint771@gmail.comgit config --global user.email colint771@gmail.com>
2023-01-23 19:27:30 -0500

    debug3

---
+++ routes/apiRoutes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const Store = require('./db/Store');
+
+router.get("/notes", function(req, res) {
+    Store
+        .getNotes()
+        .then(notes => res.json(notes))
+        .catch(err => res.status(500).json(err));
+});
+
+router.post("/notes", (req, res) => {
+    Store
+        .addNote(req.body)
+        .then((note) => res.json(note))
+        .catch(err => res.status(500).json(err));
+});
+
+router.delete("/notes/:id", function(req, res) {
+    Store
+        .removeNote(req.params.id)
+        .then(() => res.json({ ok: true }))
+        .catch(err => res.status(500).json(err));
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const apiRoutes = require('./routes/apiRoutes');
+const apiRoutes = require('./routes/apiRoutes/noteRoute');
 const htmlRoutes = require('./routes/htmlRoutes');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static('public'));
+require("routes/apiRoutes/index.js")(app);
+require("routes/apiRoutes/noteRoute.js")(app);
+require("routes/htmlRoutes/index.js")(app);
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 app.listen(PORT, () => {
